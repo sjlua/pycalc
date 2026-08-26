4a960ecba04e45a97a600e8fd463627b15a01bd8
Michael <107759642+michael-wenzi@users.noreply.github.com>
2026-08-26 18:04:05 +1000

    Add function documentation

--- calc.py
+++ calc.py
@@ -1,7 +1,9 @@
 def compute(expression):
+    """Compute a simple arithmetic expression."""
     num0, operator, num1 = expression.split(' ')
     num0 = int(num0)
     num1 = int(num1)
+
     if operator == '+':
         return num0 + num1
     elif operator == '-':
